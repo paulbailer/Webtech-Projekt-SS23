6862ac816594dc3b0156a785888ea04311cf289b
faulerpauler <page.bailer@gmx.de>
2023-04-18 12:10:45 +0200

    Initial commit

---
+++ src/test/java/htwberlin/web/tech/WebTechApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.web.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
